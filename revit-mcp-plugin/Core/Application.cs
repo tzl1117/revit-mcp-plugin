1c4777fdd2d8846e346f4a7e385e108425452c00
李锶厅(lisiting01) <lisiting01@longfor.com>
2025-04-01 10:53:25 +0800

    update icon

--- revit-mcp-plugin/Core/Application.cs
+++ revit-mcp-plugin/Core/Application.cs
@@ -13,7 +13,7 @@ namespace revit_mcp_plugin.Core
         {
             RibbonPanel mcpPanel = application.CreateRibbonPanel("Revit MCP Plugin");
 
-            PushButtonData pushButtonData = new PushButtonData("ID_EXCMD_TOGGLE_REVIT_REVIT_MCP", "Claude MCP Switch",
+            PushButtonData pushButtonData = new PushButtonData("ID_EXCMD_TOGGLE_REVIT_MCP", "Revit MCP\r\n Switch",
                 Assembly.GetExecutingAssembly().Location, "revit_mcp_plugin.Core.MCPServiceConnection");
             pushButtonData.ToolTip = "Open / Close mcp server";
             pushButtonData.Image = new BitmapImage(new Uri("/revit-mcp-plugin;component/Core/Ressources/icon-16.png", UriKind.RelativeOrAbsolute));
@@ -22,7 +22,7 @@ namespace revit_mcp_plugin.Core
 
             PushButtonData mcp_settings_pushButtonData = new PushButtonData("ID_EXCMD_MCP_SETTINGS", "Settings",
                 Assembly.GetExecutingAssembly().Location, "revit_mcp_plugin.Core.Settings");
-            mcp_settings_pushButtonData.ToolTip = "Settings";
+            mcp_settings_pushButtonData.ToolTip = "MCP Settings";
             mcp_settings_pushButtonData.Image = new BitmapImage(new Uri("/revit-mcp-plugin;component/Core/Ressources/settings-16.png", UriKind.RelativeOrAbsolute));
             mcp_settings_pushButtonData.LargeImage = new BitmapImage(new Uri("/revit-mcp-plugin;component/Core/Ressources/settings-32.png", UriKind.RelativeOrAbsolute));
             mcpPanel.AddItem(mcp_settings_pushButtonData);
